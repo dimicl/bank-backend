7e1cae2a7c2ad616336d1dbc961f5e13ce5e686d
Luka Dimic <lukagaming11@gmail.com>
2025-03-05 23:16:01 +0100

    izmena

--- Program.cs
+++ Program.cs
@@ -12,7 +12,7 @@ builder.Services.AddCors(options =>
         policy.AllowAnyHeader()
               .AllowAnyMethod()
               .AllowCredentials()
-              .WithOrigins("http://localhost:4209", "https://akulbank.netlify.app", "https://banka-l2g6.onrender.com");
+              .WithOrigins("http://localhost:4209", "https://akulbank.netlify.app", "https://akulbank.onrender.com");
     });
 });
 
@@ -31,9 +31,9 @@ if (app.Environment.IsDevelopment() || app.Environment.IsProduction())
 {
     app.UseSwagger();
     
-    app.UseSwaggerUI(options =>
+     app.UseSwaggerUI(options =>
     {
-        options.SwaggerEndpoint("/swagger/v1/swagger.json", "API v1");
+        options.SwaggerEndpoint("https://akulbank.onrender.com/swagger/v1/swagger.json", "Banka API v1");
         options.RoutePrefix = "api/docs"; 
     });
 }
--- obj/Debug/net8.0/WebTemplate.AssemblyInfo.cs
+++ obj/Debug/net8.0/WebTemplate.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebTemplate")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f01e76f696a3ae0c8bbfe2ec1a8a4278f35b9e6e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+108af602b3cb42087c9f32a6870f7f3f538475e7")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebTemplate")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebTemplate")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
