a1d2d88f55c643219f4ce29501af8e3ddbc28b28
Luka Dimic <lukagaming11@gmail.com>
2025-03-05 22:31:03 +0100

    commit

---
+++ Program.cs
@@ -0,0 +1,55 @@
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDbContext<BankaContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("CORS", policy =>
+    {
+        policy.AllowAnyHeader()
+              .AllowAnyMethod()
+              .AllowCredentials()
+              .WithOrigins("http://localhost:4209", "https://akulbank.netlify.app");
+    });
+});
+
+builder.WebHost.UseUrls("http://*:8080");
+
+builder.Services.AddControllers();
+
+builder.Services.AddScoped<AuthService>();
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    
+    app.UseSwaggerUI(options =>
+    {
+        options.SwaggerEndpoint("/swagger/v1/swagger.json", "API v1");
+        options.RoutePrefix = "api/docs"; 
+    });
+}
+
+app.UseDefaultFiles();
+app.UseStaticFiles();
+
+app.UseCors("CORS");
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.MapGet("/api/welcome", () =>
+{
+    return Results.Ok("Welcome! The application is running.");
+});
+
+app.Run();
